86d660cd66c35c25c7f2184f3f42eec594a3e5e0
Leeyoonhu <316902@naver.com>
2022-08-30 15:51:25 +0900

    make jsp

---
+++ bit_jsp/jsp_explain.java
@@ -0,0 +1,20 @@
+package bit_jsp;
+
+public class Jsp01 {
+/*
+ * 서버와 클라이언트 사이엔 connection이 먼저 
+ * 
+ * http 요청 방식 : get => header, post => body
+ * 
+ * 요청이후 200 OK => 요청 처리 성공
+ * 404 NOT FOUND => 요청한 문서가 존재하지 않음
+ * 500 Internal Server Error => 서버 프로그램 실행 시 오류
+ * 
+ * 작업 환경
+ * Dynamic Web project(새 프로젝트) >> src >> main >> webapp 
+ * window >> web 에서 html, css, jsp 기본 인코딩 UTF-8로 할것
+ * 
+ * dynamic wep project 우클릭 > properties > project facets > runtimes > Apache Tomcat v9.0 check > apply
+ */
+
+}
