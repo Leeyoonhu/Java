98bebab5c668341a0ea824581d0480f33e790a1a
Leeyoonhu <316902@naver.com>
2022-09-16 11:10:12 +0900

    Spring2

--- ex00/src/main/java/org/conan/controller/BoardController.java
+++ ex00/src/main/java/org/conan/controller/BoardController.java
@@ -35,11 +35,12 @@ public class BoardController {
 		rttr.addFlashAttribute("result", board.getBno());
 		return "redirect:/board/list";
 	}
-	@GetMapping("/get")
-	public void get(@RequestParam("bno") Long bno, Model model) {
-		log.info("/get");
-		model.addAttribute("board", service.get(bno));
-	}
+//	@GetMapping("/get")
+//	public void get(@RequestParam("bno") Long bno, Model model) {
+//		log.info("/get");
+//		model.addAttribute("board", service.get(bno));
+//	}
+//	
 	@PostMapping("/modify")
 	public String modify(BoardVO board, RedirectAttributes rttr) {
 		log.info("modify :" + board);
@@ -60,5 +61,13 @@ public class BoardController {
 	@GetMapping("/register")
 	public void register() {
 //		/board/register 주소창일때 쓰게하려고
+	}
+
+//	조회 페이지기반으로 수정도 가능하기때문에 수정도 할수있도록 /board/get, /board/modify 둘다 적용됨 
+// 	비슷한 form에서 get>> 조회 / modify>> 수정도 가능하게 기능 구현
+	@GetMapping({"/get", "/modify"})
+	public void get(@RequestParam("bno")Long bno, Model model) {
+		log.info("/get or modify");
+		model.addAttribute("board", service.get(bno));
 	}
 }
