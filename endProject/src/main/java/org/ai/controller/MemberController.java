973bed0fa3daad181d5bfbe1d832efbd8b9ea924
Leeyoonhu <316902@naver.com>
2022-10-05 21:56:30 +0900

    add naver api login

--- endProject/src/main/java/org/ai/controller/MemberController.java
+++ endProject/src/main/java/org/ai/controller/MemberController.java
@@ -9,23 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.log4j.Log4j;
 
-@Controller
-@Log4j
-@RequestMapping(value = "/member/*")
+
 public class MemberController {
 	
-	@Autowired
-	private KaKaoService kakao;
 	
-	// 카카오 로그인 테스트용 
-	@RequestMapping(value = "kakaoLogin", method=RequestMethod.GET)
-	public String kakaoLogin(@RequestParam(value="code", required = false) String code) {
-		System.out.println("Test Code : " + code);
-		
-		//  토큰 code
-		String access_Token = kakao.getAccessToken(code);
-		System.out.println("가져온 token : " + access_Token);
-		
-		return "member/testPage";
-	}
 }
