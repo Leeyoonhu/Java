60c5cd644deac5f6972c44d68ae26d31f61bec9f
Leeyoonhu <316902@naver.com>
2022-10-11 09:51:28 +0900

    add log out

--- futsal/src/main/java/com/ai/controller/LoginController.java
+++ futsal/src/main/java/com/ai/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.ai.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
 import java.util.HashMap;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -65,4 +71,60 @@ public class LoginController {
 		mav.setViewName("index");
 		return mav;
 	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView getState(HttpSession session, Model model) throws UnsupportedEncodingException {
+		// https://developers.naver.com/docs/login/api/api.md 참조
+		// 로그인 페이지시 네이버 버튼을 누르기 위해 필요한 정보
+		ModelAndView mav = new ModelAndView();
+		String clientId = "s3SKlARx4M5gtCyBNSwG";//애플리케이션 클라이언트 아이디값";
+		// callbackURL 나중에 변경할것
+	    String redirectURI = URLEncoder.encode("http://localhost:8080/login", "UTF-8");
+	    SecureRandom random = new SecureRandom();
+	    // state 는 Naver 사에서 'CSRF를 방지하기 위한 인증값입니다. 임의의 값을 넣어 진행해주시면 되는데요.' 라고 답변 (난수 입력)
+	    String state = new BigInteger(130, random).toString();
+	    String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+	    apiURL += "&client_id=" + clientId;
+	    apiURL += "&redirect_uri=" + redirectURI;
+	    apiURL += "&state=" + state;
+	    System.out.println(apiURL);
+	    mav.addObject("apiURL", apiURL);
+	    mav.setViewName("index");
+	    return mav;
+	}
+	
+	// 유저 로그아웃
+		@RequestMapping(value = "/logout")
+		public ModelAndView logout(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+			ModelAndView mav = new ModelAndView();
+			String platform = null;
+			Cookie[] cookieArr = request.getCookies();
+			for(Cookie cookie : cookieArr) {
+				if(cookie.getName().equals("platform")) {
+					platform = cookie.getValue();
+				}
+			}
+			if(platform.equals("kakao")) {
+				kakaoApi.logout((String)session.getAttribute("access_token"));
+			}
+			if(platform.equals("naver")) {
+				naverApi.logout((String)session.getAttribute("access_token"));
+			}
+			if(platform.equals("google")) {
+				googleApi.logout((String)session.getAttribute("access_token"));
+			}
+
+			// 세션 다죽임
+			session.invalidate();
+			// 쿠키도 죽임
+			for(Cookie cookie : cookieArr) {
+				cookie.setMaxAge(0);
+				response.addCookie(cookie);
+			}
+			// 메인 페이지 위치로
+			mav.setViewName("index");
+			
+			return mav;
+		}
+	
 }
