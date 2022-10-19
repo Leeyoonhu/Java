aab0dec94bc9661c7204f40fcaaa0dc1a8816fd6
Leeyoonhu <316902@naver.com>
2022-10-19 21:23:18 +0900

    add reserve site + info + map

--- futsal/src/main/java/com/ai/controller/FieldController.java
+++ futsal/src/main/java/com/ai/controller/FieldController.java
@@ -1,25 +1,32 @@
 package com.ai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.ai.domain.FieldDTO;
 import com.ai.service.FieldService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
-@RequestMapping(path = "/field")
+@Controller
+@RequestMapping(value = "/field")
 public class FieldController {
 	
 	@Autowired
 	FieldService service;
 	
-	@RequestMapping(value = "/test01")
-	public String searchTest(@RequestParam String fName) {
-		System.out.println(fName);
-		return service.selectField(fName);
+	@RequestMapping(value = "/abcd")
+//	public void test04 () {
+//		
+//	}
+	public ModelAndView searchTest(@RequestParam("id") String id) {
+		ModelAndView mav = new ModelAndView();
+		FieldDTO field = service.selectField(id);
+		mav.addObject("field", field);
+		return mav;
 	}
 }
--- futsal/src/main/java/com/ai/repository/FieldRepository.java
+++ futsal/src/main/java/com/ai/repository/FieldRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.ai.domain.FieldDTO;
 
 public interface FieldRepository extends MongoRepository<FieldDTO, String> {
-	FieldDTO findByfName(String fName);
+	FieldDTO findByid(String id);
 }
