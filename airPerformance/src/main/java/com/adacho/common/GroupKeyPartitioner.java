75d79529b5bbc6d3b8afc619299efd4948ee8217
Leeyoonhu <316902@naver.com>
2022-10-21 11:47:40 +0900

    add Composite Key, Comparator

---
+++ airPerformance/src/main/java/com/adacho/common/GroupKeyPartitioner.java
@@ -0,0 +1,16 @@
+package com.adacho.common;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+// 그룹핑 키를 파티녀서할 GroupKeyPartitioner 객체
+public class GroupKeyPartitioner extends Partitioner<DateKey, IntWritable> {
+
+	@Override
+	public int getPartition(DateKey key, IntWritable value, int numPartitions) {
+		int hash = key.getYear().hashCode();
+		int partition = hash % numPartitions;
+		return partition;
+	}
+	
+}
