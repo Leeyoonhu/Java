a8db66941b21e74c16c875b27f9dfc9b690b45c6
Leeyoonhu <316902@naver.com>
2022-10-21 09:01:09 +0900

    add marker

--- futsal/src/main/resources/static/js/map_near.js
+++ futsal/src/main/resources/static/js/map_near.js
@@ -15,11 +15,11 @@ $(document).ready(function(){
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-// var mapTypeControl = new kakao.maps.MapTypeControl();
+var mapTypeControl = new kakao.maps.MapTypeControl();
 
 // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-// map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+//kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
 // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
 var zoomControl = new kakao.maps.ZoomControl();
@@ -53,8 +53,9 @@ function displayMarker(locPosition) {
 	var imageSrc2 = "https://i.ibb.co/ky1DywQ/Kakao-Talk-20221019-214448571.png"
 	var imageSize = new kakao.maps.Size(24, 35); 
 	var imageSize2 = new kakao.maps.Size(44, 44);
-	var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize)
-	var markerImage2 = new kakao.maps.MarkerImage(imageSrc2, imageSize2)
+	var imageOption = {offset: new kakao.maps.Point(27, 69)}
+	var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+	var markerImage2 = new kakao.maps.MarkerImage(imageSrc2, imageSize2, imageOption)
     // 첫 마커를 생성합니다
     var marker = new kakao.maps.Marker({  
         map: map, 
@@ -62,11 +63,11 @@ function displayMarker(locPosition) {
         image : markerImage
     }); 
     // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
+    //kakao.maps.event.addListener(marker, 'click', function() {
         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + locPosition + '</div>');
-        infowindow.open(map, marker);
-    });
+    //    infowindow.setContent('<div style="padding:5px;font-size:12px;">' + locPosition + '</div>');
+    //    infowindow.open(map, marker);
+    //});
     
     for(var i = 0; i < lonList.length; i++){
 		var fName = fNList[i].replace("[","")
@@ -83,14 +84,23 @@ function displayMarker(locPosition) {
     	var marker2 = new kakao.maps.Marker({
 			map: map,
 			position: locPosition2,
-			image: markerImage2
+			image: markerImage2,
+			text: fName
 		})
-		var infowindow2 = new kakao.maps.InfoWindow({
-        	content: '<div style="width: 100%; padding:5px;" class="cata new">' + fName + '</div>' // 인포윈도우에 표시할 내용
-    	});
+		
+		var customOverlay = new kakao.maps.CustomOverlay({
+   			position: locPosition2,
+    		content: '<span class="info-window">' + fName + '</span>',
+    		yAnchor: 1  
+		});
+		
+		//var infowindow2 = new kakao.maps.InfoWindow({
+        //	content: 
+        //    zIndex:1
+    	//});
     	
-    	kakao.maps.event.addListener(marker2, 'mouseover', makeOverListener(map, marker2, infowindow2));
-    	kakao.maps.event.addListener(marker2, 'mouseout', makeOutListener(infowindow2));
+    	kakao.maps.event.addListener(marker2, 'mouseover', makeOverListener(map, marker2, customOverlay));
+    	kakao.maps.event.addListener(marker2, 'mouseout', makeOutListener(customOverlay));    	
     	kakao.maps.event.addListener(marker2, 'click', moveToField(fName));
     }
     // 지도 중심좌표를 접속위치로 변경합니다
@@ -103,16 +113,17 @@ function moveToField(fName){
 	}
 }
    
-function makeOverListener(map, marker, infowindow) {
+function makeOverListener(map, marker, customOverlay) {
     return function() {
-        infowindow.open(map, marker);
+		console.log(map.getLevel())
+		if(map.getLevel() < 9){
+        	customOverlay.setMap(map, marker);
+        }
     };
 }
-function makeOutListener(infowindow) {
+function makeOutListener(customOverlay) {
     return function() {
-        infowindow.close();
+        customOverlay.setMap(null);
     };
-}
-
-	
+}	
 })
