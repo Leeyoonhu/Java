7a34b761d3bda377f6016319af32059057b1f74a
Leeyoonhu <316902@naver.com>
2022-10-24 09:00:50 +0900

    rallback history2

--- futsal/src/main/java/com/ai/controller/FieldController.java
+++ futsal/src/main/java/com/ai/controller/FieldController.java
@@ -1,12 +1,20 @@
 package com.ai.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.ai.domain.FieldDTO;
 import com.ai.service.FieldService;
@@ -22,20 +30,42 @@ public class FieldController {
 	FieldService service;
 	
 	@RequestMapping(value = "/click")
-	public ModelAndView moveToField(@RequestParam("fName") String fName, RedirectAttributes redirect) {
+	public String moveToField(@RequestParam("fName") String fName, HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		FieldDTO field = service.findByfName(fName);
 		System.out.println(Integer.parseInt(field.getId()));
-		mav.addObject("id", Integer.parseInt(field.getId()));
-		mav.setViewName("redirect:reserve");
+		session.setAttribute("fieldReserve", field.getId());
+		return "redirect:/field/reserve/"+field.getId();
+	}
+	
+	@RequestMapping("/reserve/{id}")
+	public ModelAndView searchTest(@PathVariable String id, HttpSession session) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		FieldDTO field = service.findByid(id);
+		ArrayList<FieldDTO> fList = service.findAll();
+		ArrayList<String> fNList = new ArrayList<String>();
+		ArrayList<String> latList = new ArrayList<String>(); 
+		ArrayList<String> lonList = new ArrayList<String>();
+		for(int i = 0; i < fList.size(); i++) {
+			fNList.add(fList.get(i).getfName());
+			latList.add(fList.get(i).getLatitude());
+			lonList.add(fList.get(i).getLongitude());
+		}
+		mav.addObject("fNList", fNList);
+		mav.addObject("latList", latList);
+		mav.addObject("lonList", lonList);	
+		mav.addObject("field", field);
+		mav.setViewName("/field/reserve");
 		return mav;
 	}
 	
-	@RequestMapping(value = "/reserve")
-	public ModelAndView searchTest(@RequestParam("id") String id) {
-		ModelAndView mav = new ModelAndView();
-		FieldDTO field = service.findByid(id);
-		mav.addObject("field", field);
-		return mav;
+	@RequestMapping(value = "/searchField", method=RequestMethod.POST)
+	  public ModelAndView searchField(@RequestParam("fName") String fName) {
+      ModelAndView mav = new ModelAndView();
+      FieldDTO field = service.findByfName(fName);
+      mav.addObject("searchedField", field);
+      System.out.println(field.getfAddress());
+      mav.setViewName("/searchField");
+      return mav;
 	}
 }
