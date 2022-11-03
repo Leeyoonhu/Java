395e90eb4cc7165d901864493fb86129b664b29e
Leeyoonhu <106349471+Leeyoonhu@users.noreply.github.com>
2022-11-04 07:19:16 +0900

    Delete futsal directory

--- futsal/src/main/java/com/ai/repository/ReserveListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ai.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ai.domain.ReserveListDTO;
-
-public interface ReserveListRepository extends MongoRepository<ReserveListDTO, String> {
-	ReserveListDTO insert(ReserveListDTO rList);
-}
